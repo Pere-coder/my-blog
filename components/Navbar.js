662c54f58c77d50efd15099f267cac47212112be
Pere <gpere800@gmail.com>
2023-07-11 08:43:49 -0700

    modified:   components/Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,26 +12,33 @@ import { useState } from 'react';
 const Navbar = () => {
 
 const [isOpen, setIsOpen] = useState(false);
+const [Open, setOpen] = useState(false);
 
 const toggleDropdown = () => {
       setIsOpen(!isOpen);
     };
-    const click = () => {
+
+const click = () => {
         setIsOpen(isOpen);
     }
 
+
+const tDropdown = () => {
+    setOpen(!Open);
+};
+
+const klick = () => {
+    setOpen(!Open);
+}
 
     return ( <div className="flex flex-col justify-center" >
         
                 <nav className="flex  items-center text-center w-full justify-between  h-[50px] p-5">
                     <div className="text-[20px] md:text-[30px] ">Gixblog</div>
                     <ul className="hidden md:flex text-center justify-center gap-5 font-[500px]">
-                        <h1>Your one stop news and blog platform.....</h1>
-                        
+                        <h1>Your one stop news and blog platform.....</h1>  
                     </ul>
-                   
-                    {/* <div className="md:hidden"><BsSearch size={30}/></div> */}
-                    <div className="flex gap-2 bg-gray-200 w-[200px] h-[30px] p-2 justify-start items-center rounded-xl"><input  type='text' className='appearance-none border-none outline-none w-[150px] bg-gray-200' /><BsSearch  size={20}/></div>
+                    <div className="flex gap-2 bg-gray-200 w-[200px] h-[30px] p-2 justify-start items-center rounded-xl"><input  type='text' className='appearance-none border-none outline-none w-[150px] bg-gray-200' /><BsSearch className='cursor-pointer'  size={20}/></div>
                 </nav>
 
                 <nav className="flex flex-col items-center justify-center p-5 h-[120px] bg-red-500 text-white ">
@@ -77,7 +84,31 @@ const toggleDropdown = () => {
                                     </li>
                                 </ul> )}
                          </div>
-                        <li className="border-r-2 border-white w-[150px] cursor-pointer" onClick={(e) => {e.preventDefault(); setIsOpen(false)}}>Who we are</li>
+                        <div>
+                            <li className="border-r-2 border-white w-[150px] cursor-pointer ml-10" onClick={tDropdown}>Who we are</li>
+                            {Open && (
+                                <ul className="absolute mx-0 flex flex-col justify-center w-[170px] mt-2  bg-white border  shadow-lg text-sm">
+                                
+                                    <li>
+                                        <a href="#" onClick={(e) => {e.preventDefault(); setOpen(false)}} className="flex justify-center text-gray-800 hover:bg-gray-200">
+                                        About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"  onClick={(e) => {e.preventDefault(); setOpen(false)}} className="flex justify-center text-gray-800 hover:bg-gray-200">
+                                        Contact Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" onClick={(e) => {e.preventDefault(); setOpen(false)}} className="flex justify-center text-gray-800 hover:bg-gray-200">
+                                        FAQ</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" onClick={(e) => {e.preventDefault(); setOpen(false)}} className="flex justify-center text-gray-800 hover:bg-gray-200">
+                                        Career</a>
+                                    </li>
+                                 
+                                </ul> )}
+
+                        </div>
                         <li className="border-r-2 border-white w-[150px] cursor-pointer" onClick={(e) => {e.preventDefault(); setIsOpen(false)}}>Submit your article</li>
                         <li className="border-r-2 border-white w-[150px] cursor-pointer" onClick={(e) => {e.preventDefault(); setIsOpen(false)}}>Follow us</li>
                     </ul>
