3165379288df5cc6ebcbe81795343689d4234036
Pere <gpere800@gmail.com>
2023-07-20 18:43:24 -0700

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 import Email from "./Email";
 
-const footer = () => {
+const Footer = () => {
     return ( <div className="w-full h-[300px] bg-gray-700 mt-20">
                 <Email/>
             </div> );
 }
  
-export default footer;
+export default Footer;
