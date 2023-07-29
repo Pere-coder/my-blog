5dc540b6889ecff35394070f9f4f0c92110cfe96
Pere <gpere800@gmail.com>
2023-07-29 06:16:07 -0700

    modified:   components/Weatherdisplay.js

--- components/Weatherdisplay.js
+++ components/Weatherdisplay.js
@@ -43,7 +43,7 @@ export default function Weatherdisplay() {
   };
   
     return (
-      <div className='flex flex-col mx-auto mt-10 w-[300px] md:w-[700px] p-5 h-[300px] md:h-[400px] rounded-md bg-blue-500'>
+      <div className='flex flex-col mx-auto mt-10 w-[300px] md:w-[700px] p-5 h-[300px] md:h-[400px] rounded-md bg-red-500'>
   
       <div className='flex flex-col bg-black/40 '/>
          <div className='flex justify-center items-center  text-white'>
