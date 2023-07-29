9f10735ab5adf9b844f94aa1dd43f3c877ed5253
Pere <gpere800@gmail.com>
2023-07-28 20:55:28 -0700

    modified:   components/Weatherdisplay.js

--- components/Weatherdisplay.js
+++ components/Weatherdisplay.js
@@ -40,33 +40,6 @@ export default function Weatherdisplay() {
     setCity('');
     setLoading(false);
   };
-//  if (error){
-//     return  <div className='flex flex-col mx-auto mt-10 w-[300px] md:w-[700px] p-5 h-[300px] md:h-[400px] rounded-md bg-blue-500 bg-url'>
-//                 <div className=' bg-black/40 '/>
-//                     <div className='flex justify-center items-center  text-white'>
-//                     <form onSubmit={fetchWeather} className='flex  items-center w-[200px] h-[30px] rounded-md p-5 bg-transparent border border-gray-300 text-black '>
-//                         <div className='flex gap-2'>
-//                         <input onChange={(e) => setCity(e.target.value)}
-//                         className='w-[120px] bg-transparent border-none text-white focus:outline-none text-[20px]' type="text" placeholder='Search city'/>
-//                         <button onClick={fetchWeather}><BsSearch size={20} className='text-white'/></button>
-//                         </div>
-                       
-//                     </form>
-//                 <div className='text-red-500 absolute text-[15px] mt-[400px] w-[300px] bg-black/50 p-2 rounded-md'><div className='m-5 flex justify-center items-center hover:cursor-pointer '></div>{error}.......
-//                     Please enter a valid city or check your internet connection</div>
-//                 </div>
-       
-  
-
-//          {weather.main && <Weather data={weather}/>}
-  
-  
-  
-  
-  
-//       </div>
-  
-//   }
   
     return (
       <div className='flex flex-col mx-auto mt-10 w-[300px] md:w-[700px] p-5 h-[300px] md:h-[400px] rounded-md bg-blue-500'>
