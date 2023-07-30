d7793b4e315d72103ef2d2a46f6e92b7f85bbeea
Pere <gpere800@gmail.com>
2023-07-29 18:14:40 -0700

    modified:   app/read/[id]/page.js

--- app/read/[id]/page.js
+++ app/read/[id]/page.js
@@ -45,7 +45,7 @@ const Blogdetail = () => {
                     <div className="w-[250px] h-[100px] md:w-[600px] md:h-[400px] mx-auto flex items-center justify-center mt-10 bg-no-repeat bg-cover bg-center rounded-md" style={{
                          backgroundImage: `url(${data.image})`,
                     }}></div>
-                    <p className="mt-7 text-[10px] md:text-[20px] p-5 ">{data.blog}</p>
+                    <p className="mt-7 text-[10px] md:text-[20px] p-5 font-sans">{data.blog}</p>
                     <Like/>
 
                     <Comment/>
