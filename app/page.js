8b657706ddee8790194970cf419734ab44133885
Pere <gpere800@gmail.com>
2023-07-29 19:33:47 -0700

    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Footer from '@/components/Footer'
 import Blog from '@/components/Blog'
 import Image from 'next/image'
 import Weatherdisplay from '@/components/Weatherdisplay'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+
 
 
 
@@ -12,6 +13,13 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
       <div>
+      <Head>
+        <title>Blog | Gixblog</title>
+        <meta
+          name="description"
+          content="This is a blog and news article page."
+        />
+      </Head>
         <Navbar/>
         <Weatherdisplay/>
         <Blog/>
